ca0f0d7238d794c8497e0509158de9ea4a14b7c1
Dynamo <chickabamboombam@gmail.com>
2025-05-22 02:41:26 -0400

    Updated StarterPlayer/StarterCharacterScripts/SprintScript.lua

---
+++ StarterPlayer/StarterCharacterScripts/SprintScript.lua
@@ -0,0 +1,28 @@
+-- @ScriptType: LocalScript
+local mouse = game.Players.LocalPlayer:GetMouse()
+local running = false
+
+
+mouse.KeyDown:connect(function (key)
+	key = string.lower(key)
+	if string.byte(key) == 48 then
+		running = true
+		local keyConnection = mouse.KeyUp:connect(function (key)
+			if string.byte(key) == 48 then
+				running = false
+			end
+		end)
+		for i = 1,5 do
+			game.Workspace.CurrentCamera.FieldOfView = (70+(i*2))
+			wait()
+		end
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 35
+		repeat wait () until running == false
+		keyConnection:disconnect()
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+		for i = 1,5 do
+			game.Workspace.CurrentCamera.FieldOfView = (80-(i*2))
+			wait()
+		end
+	end
+end)
